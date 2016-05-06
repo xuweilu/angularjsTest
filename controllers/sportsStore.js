385451dac2402ec47ead2f3601c4b7c0c535666e
zhibeiyou <xuweiluwx@gmail.com>
2016-05-06 18:55:42 +0800

    add a post method

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,7 +1,8 @@
 ///<refrence path='../angular.js'/>
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:5500/products")
-    .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    .constant("orderUrl", "http://localhost:5500/orders")
+    .controller("sportsStoreCtrl", function ($scope, $http, $location, dataUrl, orderUrl, cart) {
         $scope.data = {};
         $http.get(dataUrl)
             .success(function (data) {
@@ -9,5 +10,19 @@ angular.module("sportsStore")
             })
             .error(function (error) {
                 $scope.data.error = error;
-            })
+            });
+        $scope.sendOrder = function (shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function (data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function (error) {
+                    $scope.data.orderError = error;
+                }).finally(function () {
+                    $location.path("/complete");
+                });
+        }
     });
