4863291cf48419989bd227eadbdcc6fbb4eeaa50
zhibeiyou <xuweiluwx@gmail.com>
2016-05-06 14:00:04 +0800

    add a basic shopping cart

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -2,7 +2,7 @@
 angular.module("sportsStore")
     .constant("productListActiveClass", "btn-primary")
     .constant("productListPageCount", 3)
-    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount) {
+    .controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
         var selectedCategory = null;
         $scope.selectedPage = 1;
         $scope.pageSize = productListPageCount;
@@ -23,4 +23,7 @@ angular.module("sportsStore")
         $scope.getPageClass = function (page) {
             return $scope.selectedPage == page ? productListActiveClass : "";
         }
-    })
+        $scope.addProductToCart = function (product) {
+            cart.addProduct(product.id, product.name, product.price);
+        }
+    });
