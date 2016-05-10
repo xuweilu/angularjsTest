f4cccd64255e7b747f05bfeaba26aed668ef5e0b
zhibeiyou <xuweiluwx@gmail.com>
2016-05-10 16:03:29 +0800

    $http ajax test

--- products.js
+++ products.js
@@ -1,4 +1,4 @@
-angular.module("exampleApp", [])
+angular.module("exampleApp", ["increment"])
     .constant("baseUrl", "http://localhost:5500/products/")
     .controller("defaultCtrl", function ($scope, $http, baseUrl) {
 
@@ -6,44 +6,41 @@ angular.module("exampleApp", [])
         $scope.currentProduct = null;
 
         $scope.listProducts = function () {
-            $scope.products = [
-                {
-                    id: 0,
-                    name: "Dummy1",
-                    category: "Test",
-                    price: 1.25
-                },
-                {
-                    id: 1,
-                    name: "Dummy2",
-                    category: "Test",
-                    price: 2.45
-                },
-                {
-                    id: 2,
-                    name: "Dummy3",
-                    category: "Test",
-                    price: 4.25
-                }];
+            $http.get(baseUrl).success(function (data) {
+                $scope.products = data;
+            })
         }
 
         $scope.deleteProduct = function (product) {
-            $scope.products.splice($scope.products.indexOf(product), 1);
+            $http({
+                method: "DELETE",
+                url: baseUrl + product.id
+            }).success(function () {
+                $scope.products.splice($scope.products.indexOf(product), 1);
+            });
         }
 
         $scope.createProduct = function (product) {
-            $scope.products.push(product);
-            $scope.displayMode = "list";
+            $http.post(baseUrl, product).success(function (newProduct) {
+                $scope.products.push(product);
+                $scope.displayMode = "list";
+            });
         }
 
         $scope.updateProduct = function (product) {
-            for (var i = 0; i < $scope.products.length; i++) {
-                if ($scope.products[i].id == product.id) {
-                    $scope.products[i] = product;
-                    break;
+            $http({
+                url: baseUrl + product.id,
+                method: "PUT",
+                data: product
+            }).success(function (modifiedProduct) {
+                for (var i = 0; i < $scope.products.length; i++) {
+                    if ($scope.products[i].id == modifiedProduct.id) {
+                        $scope.products[i] = modifiedProduct;
+                        break;
+                    }
                 }
-            }
-            $scope.displayMode = "list";
+                $scope.displayMode = "list";
+            });
         }
 
         $scope.editOrCreateProduct = function (product) {
